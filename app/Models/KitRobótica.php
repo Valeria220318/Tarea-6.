4f58679ee235fe770aeefcf7136e0c1ef816f86e
Valeria220318 <al03045432@tecmilenio.mx>
2025-03-08 20:12:40 -0600

    Database setup with Eloquent ORM

--- app/Models/KitRobótica.php
+++ app/Models/KitRobótica.php
@@ -5,7 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class KitRobótica extends Model
+class KitsRobotica extends Model
 {
     use HasFactory;
+
+    // Relación entre KitsRobotica y Curso
+    public function cursos()
+    {
+        return $this->hasMany(Curso::class, 'kitDeRobot_id');
+    }
 }
