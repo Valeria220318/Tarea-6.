77f2cb36843b1f0997370b63322c2bdb5a590341
Valeria220318 <al03045432@tecmilenio.mx>
2025-03-08 19:45:05 -0600

    Update

--- database/migrations/2025_03_08_044016_create_grupos_table.php
+++ database/migrations/2025_03_08_044016_create_grupos_table.php
@@ -30,6 +30,22 @@ class CreateGruposTable extends Migration
     }
 }
 
+class Grupo extends Model
+{
+    use HasFactory;
+
+    // Relación: Un grupo tiene muchos usuarios
+    public function usuarios()
+    {
+        return $this->hasMany(Usuario::class);
+    }
+
+    // Relación de muchos a muchos: Un grupo tiene muchos cursos
+    public function cursos()
+    {
+        return $this->belongsToMany(Curso::class, 'grupos_cursos');
+    }
+}
 
 
 
