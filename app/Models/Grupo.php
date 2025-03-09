bdc4bf397c7af19cf7218f5980ede0326f3576ad
Valeria220318 <al03045432@tecmilenio.mx>
2025-03-08 20:11:53 -0600

    Database setup with Eloquent ORM

--- app/Models/Grupo.php
+++ app/Models/Grupo.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Grupo extends Model
 {
     use HasFactory;
+
+    // Relación: Un grupo tiene muchos usuarios
+    public function usuarios()
+    {
+        return $this->hasMany(Usuario::class);
+    }
+
+    // Relación de muchos a muchos: Un grupo tiene muchos cursos
+    public function cursos()
+    {
+        return $this->belongsToMany(Curso::class, 'grupos_cursos');
+    }
 }
