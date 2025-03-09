270d6d658fc1ccca1e8f8c74f463d1daa518dd86
Valeria220318 <al03045432@tecmilenio.mx>
2025-03-08 19:57:54 -0600

    Update

--- database/migrations/2025_03_08_044007_create_usuarios_table.php
+++ database/migrations/2025_03_08_044007_create_usuarios_table.php
@@ -32,20 +32,6 @@ class CreateUsuariosTable extends Migration
     }
 }
 
-class Usuario extends Model
-{
-    //Relación de muchos a muchos: Un curso pertenece a muchos grupos.
-    public function grupo()
-    {
-        return $this->belongsTo(Grupo::class, 'grupo_id');
-    }
-
-    //Relación de uno a muchos: Un curso tiene muchos materiales didácticos.
-    public function materiales()
-    {
-        return $this->hasMany(MaterialDidactico::class);
-    }
-}
 
 
 
