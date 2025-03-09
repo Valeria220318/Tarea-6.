e17c7d335b19c4e66c2552b2ee426646f2da6633
Valeria220318 <al03045432@tecmilenio.mx>
2025-03-08 20:06:24 -0600

    Database setup with Eloquent ORM

--- app/Models/Curso.php
+++ app/Models/Curso.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Curso extends Model
 {
     use HasFactory;
+
+    // Relación de muchos a muchos: Un curso pertenece a muchos grupos
+    public function grupos()
+    {
+        return $this->belongsToMany(Grupo::class, 'grupos_cursos');
+    }
+
+    // Relación uno a muchos inversa: Un curso pertenece a un kit de robótica
+    public function kitDeRobot()
+    {
+        return $this->belongsTo(KitsRobotica::class, 'kitDeRobot_id');
+    }
 }
