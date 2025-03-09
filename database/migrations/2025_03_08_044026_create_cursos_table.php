72f9dafed7e6463b8601b2dd8326ed12ac008a68
Valeria220318 <al03045432@tecmilenio.mx>
2025-03-08 19:45:23 -0600

    Update

--- database/migrations/2025_03_08_044026_create_cursos_table.php
+++ database/migrations/2025_03_08_044026_create_cursos_table.php
@@ -30,6 +30,22 @@ class CreateCursosTable extends Migration
     }
 }
 
+class Curso extends Model
+{
+    use HasFactory;
+
+    // Relación de muchos a muchos: Un curso pertenece a muchos grupos
+    public function grupos()
+    {
+        return $this->belongsToMany(Grupo::class, 'grupos_cursos');
+    }
+
+    // Relación uno a muchos inversa: Un curso pertenece a un kit de robótica
+    public function kitDeRobot()
+    {
+        return $this->belongsTo(KitsRobotica::class, 'kitDeRobot_id');
+    }
+}
 
 
 
